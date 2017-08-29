d37f5dfc7d280fe639311f3f4f4b04178c0729c3
Antti Leppä <antti.leppa@metatavu.fi>
2017-08-29 16:23:37 +0300

    Fixed issue with vendor loading

--- wordpress-linkedevents.php
+++ wordpress-linkedevents.php
@@ -3,7 +3,7 @@
  * Created on Aug 22, 2018
  * Plugin Name: Linked Events
  * Description: Wordpress plugin to manage Linked Events API
- * Version: 0.0.1
+ * Version: 1.0.1
  * Author: Metatavu Oy
  */
 
