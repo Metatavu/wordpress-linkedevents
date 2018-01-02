eec8498b409a895e6eae8ca1f1872abba3aea29e
Antti Leppä <antti.leppa@metatavu.fi>
2018-01-02 17:18:27 +0200

    Update wordpress-linkedevents.php

--- wordpress-linkedevents.php
+++ wordpress-linkedevents.php
@@ -3,7 +3,7 @@
  * Created on Aug 22, 2018
  * Plugin Name: Linked Events
  * Description: Wordpress plugin to manage Linked Events API
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: Metatavu Oy
  */
 
