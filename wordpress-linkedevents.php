af20dbb1fcd1dcffd03c7d7d41f4469372f07320
Antti Leppä <antti.leppa@metatavu.fi>
2018-05-16 14:30:00 +0300

    fixed issue with missing main file

---
+++ wordpress-linkedevents.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created on Aug 22, 2018
+ * Plugin Name: Linked Events
+ * Description: Wordpress plugin to manage Linked Events API
+ * Version: 1.0.10
+ * Author: Metatavu Oy
+ */
+
+  defined ( 'ABSPATH' ) || die ( 'No script kiddies please!' );
+
+  require_once( __DIR__ . '/linkedevents/linkedevents.php');
+  require_once( __DIR__ . '/settings/settings.php');
+  require_once( __DIR__ . '/notifications/notifications.php');
+
+  add_action('plugins_loaded', function() {
+    load_plugin_textdomain('linkedevents', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+  });
+  
+  register_activation_hook(__FILE__, ['\Metatavu\LinkedEvents\Wordpress\Capabilities', 'activationHook']);
+  register_deactivation_hook(__FILE__, ['\Metatavu\LinkedEvents\Wordpress\Capabilities', 'deactivationHook']);
+  
+?>
