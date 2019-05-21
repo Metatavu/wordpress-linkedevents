64209c754a166f70001529e93165c338bfebf5f0
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-21 11:43:44 +0300

    Added template dependency

--- wordpress-linkedevents.php
+++ wordpress-linkedevents.php
@@ -3,16 +3,21 @@
  * Created on Aug 22, 2018
  * Plugin Name: Linked Events
  * Description: Wordpress plugin to manage Linked Events API
- * Version: 1.0.11
+ * Version: 2.0.0
  * Author: Metatavu Oy
  */
 
   defined ( 'ABSPATH' ) || die ( 'No script kiddies please!' );
 
+  if (!defined('LINKEDEVENTS_PLUGIN_DIR')) {
+    define('LINKEDEVENTS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+  }
+
   require_once( __DIR__ . '/linkedevents/linkedevents.php');
   require_once( __DIR__ . '/settings/settings.php');
   require_once( __DIR__ . '/notifications/notifications.php');
-
+  require_once( __DIR__ . '/gutenberg/gutenberg.php');
+  
   add_action('plugins_loaded', function() {
     load_plugin_textdomain('linkedevents', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
   });
