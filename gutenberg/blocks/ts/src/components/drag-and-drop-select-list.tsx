cf41350ec0c199e4a4ad31fe214482ff2cd32c0e
Julius Saarikko <julius.saarikko@dymesolutions.com>
2020-05-29 15:44:45 +0300

    Drag and Drop component improvements

--- gutenberg/blocks/ts/src/components/drag-and-drop-select-list.tsx
+++ gutenberg/blocks/ts/src/components/drag-and-drop-select-list.tsx
@@ -4,14 +4,48 @@ import { wp } from 'wp';
 declare var wp: wp;
 const { __ } = wp.i18n;
 
-class DragAndDropSelectList extends React.Component {
-  state = {
-    items: [
-      { name: 'name', selected: true },
-      { name: 'shortDescription', selected: false },
-      { name: 'location', selected: false }
-    ]
-  };
+export interface DragAndDropListItem {
+  name: string;
+  selected: boolean;
+}
+
+interface State {
+  items: DragAndDropListItem[];
+  selected: string[];
+}
+
+interface Props {
+  value: string[];
+  onChange: (value: string[]) => void;
+}
+
+class DragAndDropSelectList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      items: [
+        { name: 'name', selected: true },
+        { name: 'shortDescription', selected: false },
+        { name: 'location', selected: false }
+      ],
+      selected: ['name']
+    };
+  }
+
+    /**
+   * Component did mount life-cycle event
+   */
+  public componentDidMount = async () => {
+    const selected: any[] = [];
+
+    for (let i = 0; i < this.props.value.length; i++) {
+      selected.push({name: this.props.value[i], selected: true});
+    }
+
+    this.setState({
+      selected: selected
+    });
+  }
 
   draggedItem: any;
   draggedIdx: any;
@@ -41,20 +75,25 @@ class DragAndDropSelectList extends React.Component {
   handleChange(event: any) {
     const target = event.target;
     const name = target.name;
-
+    let items;
+    let selected = [];
     if (name !== 'name') {
-      this.setState({
-        items: this.state.items.map((item) => {
-          if (item.name === name) {
-             item.selected = !item.selected;
-          }
-          return item;
-        })
+      items = this.state.items.map(item => {
+        if (item.name === name) {
+          item.selected = !item.selected;
+          selected.push(item.name);
+        }
+        return item;
       });
     }
+
+    this.setState({
+      items: items
+    });
   }
 
   render() {
+    const { CheckboxControl } = wp.components;
     return (
       <div className='App'>
         <main>
@@ -63,7 +102,7 @@ class DragAndDropSelectList extends React.Component {
               <li key={item.name} onDragOver={() => this.onDragOver(idx)}>
                 <div className='drag' draggable onDragStart={e => this.onDragStart(e, idx)} onDragEnd={this.onDragEnd}>
                   <span>
-                    <input name={item.name} type='checkbox' checked={item.selected} onChange={(event) => (this.handleChange(event))} />
+                    <input name={item.name} type='checkbox' checked={item.selected} onChange={event => this.handleChange(event)} />
                   </span>
                   <span className='content'>
                     {idx} - {item.name}
